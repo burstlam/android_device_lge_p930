772047b49d2578445f94ce12e6f14b4a1329c3e3
Tom Marshall <tdm.code@gmail.com>
2013-02-28 22:56:42 +0000

    p930: initial bringup
    Remove CM specific stuff, add AOKP stuff.
    
    Change-Id: Ibcf3bcfde29ef9dc74f8622a91e2d06faeb22945

--- p930.mk
+++ p930.mk
@@ -2,6 +2,8 @@ $(call inherit-product, device/lge/iprj-common/iprj.mk)
 
 $(call inherit-product-if-exists, vendor/lge/p930/p930-vendor.mk)
 
+DEVICE_PACKAGE_OVERLAYS += device/lge/p930/overlay
+
 ## These are different between models
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
