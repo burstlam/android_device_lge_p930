43fcf4a500451d84c3ca2ad5ddd810e665edff76
Californication <miroslav_mm@ukr.net>
2013-03-18 22:23:38 +0200

    remove WiFiDirectDemo, cleanup

--- p930.mk
+++ p930.mk
@@ -6,6 +6,6 @@ $(call inherit-product-if-exists, vendor/lge/p930/p930-vendor.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/bcmdhd.cal:system/etc/wifi/bcmdhd.cal 
 
-PRODUCT_NAME := full_p930 
-PRODUCT_DEVICE := p930 
-PRODUCT_MODEL := LG-P930 
+PRODUCT_NAME := full_p930
+PRODUCT_DEVICE := p930
+PRODUCT_MODEL := LG-P930
